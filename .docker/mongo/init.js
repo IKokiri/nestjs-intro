e4b7cc15684b7472752590d7a8f850daf7e421a5
Luiz Mendes <luiz.mendes@in8.com.br>
2021-12-04 14:53:47 -0300

    feat: find all routes

---
+++ .docker/mongo/init.js
@@ -0,0 +1,20 @@
+db.routes.insertMany([
+  {
+    _id: '1',
+    title: 'Primeiro',
+    startPosition:{lat: -1, lng: 2},
+    endPosition:{lat: -1, lng: 2}
+  },
+  {
+    _id: '2',
+    title: 'Segundo',
+    startPosition:{lat: -1, lng: 2},
+    endPosition:{lat: -1, lng: 2}
+  },
+  {
+    _id: '3',
+    title: 'Terceiro',
+    startPosition:{lat: -1, lng: 2},
+    endPosition:{lat: -1, lng: 2}
+  },
+])
